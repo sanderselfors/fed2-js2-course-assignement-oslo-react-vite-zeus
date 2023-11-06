d7134e7fb6c6d55c9c79cb565201c1d26df07aef
AndreaLindaas <112169120+AndreaLindaas@users.noreply.github.com>
2023-11-06 20:25:38 +0100

    cleaned files and fixed configuration

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import { Outlet } from "@tanstack/react-router";
 import Navbar from "./components/navbar";
-import Footer from "./components/navbar/footer"
+import Footer from "./components/navbar/footer";
 import "./App.css";
 
 function App() {
+  const title = "Tittel";
   return (
     <>
       <header>
